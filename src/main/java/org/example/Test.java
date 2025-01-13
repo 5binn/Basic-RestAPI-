8e18b2aba81d48b88a2baad5514d3c5e00254a13
5binn <dhxnejqls13@gmail.com>
2025-01-13 17:59:01 +0900

    start!

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Test {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String test() {
+
+        return "dddddddddddd";
+    }
+}
